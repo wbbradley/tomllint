b124c101f6450224e4159ad1f4400eb84aeca9eb
Chris Herborth <chrish@taffer.ca>
2026-01-17 11:17:25 -0500

    Now supports multiple files per command-line

--- src/tomllint/main.py
+++ src/tomllint/main.py
@@ -7,7 +7,7 @@ from typing import BinaryIO, NamedTuple
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument("input", help='filename or "-" for stdin')
+    parser.add_argument("input", help='filename or "-" for stdin', nargs='+')
     return parser
 
 
@@ -17,30 +17,31 @@ class State(NamedTuple):
     from_stdin: bool
 
 
-def parse_args_into_state() -> State:
+def parse_args_into_state() -> list[State]:
     parser = get_parser()
     args = parser.parse_args()
     from_stdin = args.input == "-"
-    return State(
-        filename="<stdin" if from_stdin else args.input,
-        file=sys.stdin.buffer if from_stdin else open(args.input, "rb"),
+    return [State(
+        filename="<stdin" if from_stdin else x,
+        file=sys.stdin.buffer if from_stdin else open(x, "rb"),
         from_stdin=from_stdin,
-    )
+    ) for x in args.input]
 
 
 def main() -> None:
     state = parse_args_into_state()
 
-    try:
-        tomllib.load(state.file)
-    except tomllib.TOMLDecodeError as e:
-        # This is dumb. I should find a better TOML library, but this one is builtin.
-        m = re.match(r"(.*)\(at line (\d+), column (\d+)\)", str(e))
-        if not m:
-            print(e, file=sys.stderr)
-        else:
-            print(f"{state.filename}:{m[2]}:{m[3]}: error: {m[1].strip()}", file=sys.stderr)
-        sys.exit(1)
+    for item in state:
+        try:
+            tomllib.load(item.file)
+        except tomllib.TOMLDecodeError as e:
+            # This is dumb. I should find a better TOML library, but this one is builtin.
+            m = re.match(r"(.*)\(at line (\d+), column (\d+)\)", str(e))
+            if not m:
+                print(e, file=sys.stderr)
+            else:
+                print(f"{item.filename}:{m[2]}:{m[3]}: error: {m[1].strip()}", file=sys.stderr)
+            sys.exit(1)
 
 
 if __name__ == "__main__":
