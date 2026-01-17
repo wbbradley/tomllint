ecd2d45592e90853c483190ba7496e511e403088
Will Bradley <williambbradley@gmail.com>
2026-01-17 14:56:30 -0700

    add support for multiple files and a pre-commit-hook

--- src/tomllint/main.py
+++ src/tomllint/main.py
@@ -2,45 +2,52 @@ import argparse
 import re
 import sys
 import tomllib
-from typing import BinaryIO, NamedTuple
+from typing import List, BinaryIO, NamedTuple
 
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument("input", help='filename or "-" for stdin')
+    parser.add_argument("toml_file", help='TOML filenames or "-" for stdin', nargs="+")
     return parser
 
 
-class State(NamedTuple):
-    file: BinaryIO
-    filename: str
+class Args(NamedTuple):
+    filenames: List[str]
     from_stdin: bool
 
 
-def parse_args_into_state() -> State:
+def get_args() -> Args:
     parser = get_parser()
     args = parser.parse_args()
-    from_stdin = args.input == "-"
-    return State(
-        filename="<stdin>" if from_stdin else args.input,
-        file=sys.stdin.buffer if from_stdin else open(args.input, "rb"),
+    from_stdin = args.toml_file == ["-"]
+    return Args(
+        filenames=["<stdin>"] if from_stdin else args.toml_file,
         from_stdin=from_stdin,
     )
 
 
-def main() -> None:
-    state = parse_args_into_state()
-
+def check_file(file: BinaryIO, filename: str) -> int:
     try:
-        tomllib.load(state.file)
+        tomllib.load(file)
     except tomllib.TOMLDecodeError as e:
         # This is dumb. I should find a better TOML library, but this one is builtin.
         m = re.match(r"(.*)\(at line (\d+), column (\d+)\)", str(e))
         if not m:
             print(e, file=sys.stderr)
         else:
-            print(f"{state.filename}:{m[2]}:{m[3]}: error: {m[1].strip()}", file=sys.stderr)
-        sys.exit(1)
+            print(f"{filename}:{m[2]}:{m[3]}: error: {m[1].strip()}", file=sys.stderr)
+        return 1
+
+
+def main() -> None:
+    args = get_args()
+    error_code = 0
+    if args.from_stdin:
+        error_code |= check_file(file=sys.stdin.buffer, filename="<stdin>")
+    else:
+        for filename in args.filenames:
+            error_code |= check_file(file=open(filename, "rb"), filename=filename)
+    sys.exit(error_code)
 
 
 if __name__ == "__main__":
